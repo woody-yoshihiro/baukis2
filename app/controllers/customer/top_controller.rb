dcf913e4e8edf875875395a1d10efc43c5bf7275
woody-yoshihiro <woody19810930@gmail.com>
2020-06-01 14:27:45 +0900

    create

--- app/controllers/customer/top_controller.rb
+++ app/controllers/customer/top_controller.rb
@@ -1,4 +1,7 @@
-class Customer::TopController < ApplicationController
+class Customer::TopController < Customer::Base
+  skip_before_action :authorize
+
   def index
+    render action: "index"
   end
 end
