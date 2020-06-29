891532c9a3096b4c4bc10d20e219efd565ad3389
woody-yoshihiro <woody19810930@gmail.com>
2020-06-29 14:49:04 +0900

    create

---
+++ app/javascript/packs/customer.js
@@ -0,0 +1,6 @@
+require("@rails/ujs").staff()
+require("turbolinks").staff()
+require("@rails/activestorage").staff()
+require("channels")
+
+import "../customer/account_form.js";
---
+++ spec/requests/customer/accounts_request_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "Customer::Accounts", type: :request do
+
+end
